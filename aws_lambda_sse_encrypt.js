e65a4842f3afc2d2bdfbbc5761d67636938069de
Bernhard Seeger <blseeger@gmail.com>
2015-10-27 23:31:48 +0100

    Update aws_lambda_sse_encrypt.js

--- aws_lambda_sse_encrypt.js
+++ aws_lambda_sse_encrypt.js
@@ -3,26 +3,26 @@ var s3 = new aws.S3({ apiVersion: '2006-03-01' });
 exports.handler = function(event, context) {
     var params = {};
     params.Bucket = event.Records[0].s3.bucket.name;
-    params.Key = event.Records[0].s3.object.key;
+    params.Key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     s3.headObject(params, function(err, data) {
-         if (err) {
-            context.fail('Error getting ' + params.Bucket + params.Key + ':' + JSON.stringify(err));
-          } else {
-              if (data.ServerSideEncryption !== 'AES256') {
-                params.CopySource = params.Bucket + '/' + params.Key; 
-                params.ServerSideEncryption = 'AES256';
-                s3.copyObject(params, function(err,data) {
-                  if (err) {
-                      context.fail('Encryption of ' + params.CopySource + ' not successfull');
-                  } else {
-                      console.log('Encryption of ' + params.CopySource + ' successfull');
-                      context.succeed('Encryption of ' + params.CopySource + ' successfull');
-                  }
+        if (err) {
+            context.fail('Error getting ' + params.Bucket + '/' + params.Key + ': ' + JSON.stringify(err));
+        } else {
+            params.CopySource = params.Bucket + '/' + params.Key;
+            params.ServerSideEncryption = 'AES256';
+            if (data.ServerSideEncryption !== params.ServerSideEncryption) {
+                s3.copyObject(params, function(err, data) {
+                    if (err) {
+                        context.fail('Encryption of ' + params.CopySource + ' not successfull');
+                    } else {
+                        console.log('Encryption of ' + params.CopySource + ' successfull');
+                        context.succeed('Encryption of ' + params.CopySource + ' successfull');
+                    }
                 });
-              } else {
-                  console.log('No Encryption necessary for ' + params.Bucket + params.Key);
-                  context.succeed('No Encryption necessary for ' + params.Bucket + params.Key);
-              }
-          }
+            } else {
+                console.log('No Encryption necessary for ' + params.CopySource);
+                context.succeed('No Encryption necessary for ' + params.CopySource);
+            }
+        }
     });
 };
