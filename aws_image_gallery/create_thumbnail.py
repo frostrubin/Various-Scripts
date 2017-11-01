a23a0ef29eaa181acc51d84f4384ab57ac2ab85e
Bernhard Wünsch <frostrubin@gmail.com>
2017-11-01 15:29:40 +0100

    added exif orientation rotation

--- aws_image_gallery/create_thumbnail.py
+++ aws_image_gallery/create_thumbnail.py
@@ -16,6 +16,7 @@
 ## source ~/shrink_venv/bin/activate
 ## Install libraries in the virtual environment
 ## pip install Pillow
+## pip install exifread
 ## pip install boto3
 ## Note !!!
 ## AWS Lambda includes the AWS SDK for Python (Boto 3), so you don't need to include it in 
@@ -27,22 +28,21 @@
 ## Add your python code to the .zip file
 ## cd ~
 ## zip -g CreateThumbnail.zip CreateThumbnail.py
+## Transfer it to your local computer
+## scp -i key.pem ec2-user@public-ip-address:~/CreateThumbnail.zip ./CreateThumbnail.zip
 
 import boto3
 import os
 import sys
 import uuid
 from PIL import Image
+from PIL import ImageOps
 import PIL.Image
+import PIL.ImageOps
+import exifread
+from exifread.tags import EXIF_TAGS
 
 s3_client = boto3.client('s3')
-
-
-def splitext_(path):
-    if len(path.split('.')) > 2:
-        return path.split('.')[0],'.'.join(path.split('.')[-2:])
-    return os.path.splitext(path)
-
 
 def handler(event, context):
     result = {'status': 1, 'msg': 'Source or Target not fully specified','info':''}
@@ -59,6 +59,7 @@ def handler(event, context):
         target_width  = 0
         target_height = 0
         target_factor = 0
+        auto_rotate = False
 
         try:
             target_width  = event['target_width']
@@ -75,21 +76,79 @@ def handler(event, context):
         except KeyError:
             pass
 
+        try:
+            auto_rotate = event['auto_rotate']
+        except KeyError:
+            pass        
+
         result['msg'] = 'No target size specified'
         if not target_height and not target_width and not target_factor:
             raise KeyError('dummy')
 
         guid = uuid.uuid4()
-        file_name, extension = splitext_(source_key)
-        download_path = '/tmp/{}.{}'.format(guid, extension)
-        upload_path   = '/tmp/resized-{}.{}'.format(guid, extension)
+        try:
+            extension = os.path.splitext(source_key)[1]
+        except Exception as e:
+            print(e)
+            extension = 'jpg'
+
+        download_path = '/tmp/{}.{}'.format(guid, extension).replace('..','.')
+        upload_path   = '/tmp/resized-{}.{}'.format(guid, extension).replace('..','.')
         
         result['msg'] = 'Could not download file'
         s3_client.download_file(source_bucket, source_key, download_path)
         result['msg'] = 'Could not resize file'
         with Image.open(download_path) as image:
-            source_width  = image.size[0]
-            source_height = image.size[1]
+            new_image = image
+            
+            # If needed, rotate
+            if auto_rotate == True and extension.lower() in ['.jpg','.jpeg','.tiff','.tif']:
+                file = open(download_path, 'rb')
+                orientation = 1
+
+                ## Build Map for Re-Mapping of String to EXIF Orientation Integer
+                orientation_map = {}
+                for tag, tag_value in EXIF_TAGS.items():
+                    try:
+                        if tag_value[0] == 'Orientation':
+                            for key in tag_value[1]:
+                                orientation_map[tag_value[1][key]] = key
+                    except KeyError:
+                        pass
+
+                ## Get EXIF Orientation
+                try:
+                    exif_tags = exifread.process_file(file, details=False, stop_tag='Image Orientation')
+                    #print(exif_tags)
+                    orientation_string = exif_tags['Image Orientation'].printable
+                    orientation = orientation_map[orientation_string]
+                except Exception as e:
+                    pass
+
+                result['exif'] = 'Detected Orientation {}'.format(orientation)
+
+                if orientation == 1:
+                    pass
+                elif orientation == 2:
+                    new_image = ImageOps.mirror(image)
+                elif orientation == 3:
+                    new_image = image.rotate(180, expand=True)
+                elif orientation == 4:
+                    new_image = ImageOps.flip(image)
+                elif orientation == 5:
+                    new_image = image.rotate(270, expand=True)
+                    new_image = ImageOps.mirror(new_image)
+                elif orientation == 6:
+                    new_image = image.rotate(270, expand=True)
+                elif orientation == 7:
+                    new_image = image.rotate(90, expand=True)
+                    new_image = ImageOps.mirror(new_image)
+                elif orientation == 8:
+                    new_image = image.rotate(90, expand=True)            
+
+
+            source_width  = new_image.size[0]
+            source_height = new_image.size[1]
             if target_factor != 0:
                 result['info'] = 'Target Factor {}'.format(target_factor)
                 target_width  = int(source_width  / target_factor)
@@ -104,11 +163,11 @@ def handler(event, context):
                 target_width = int(source_width * target_height / source_height)
 
             if target_width > source_width or target_height > source_height:
-                resized = image.resize((target_width, target_height))
-                resized.save(upload_path)
+                new_image = new_image.resize((target_width, target_height))
             else:
-                image.thumbnail((target_width, target_height))
-                image.save(upload_path)
+                new_image.thumbnail((target_width, target_height))
+
+            new_image.save(upload_path)
         result['msg'] = 'Could not upload file'
         s3_client.upload_file(upload_path, target_bucket, target_key, {'ServerSideEncryption': 'AES256'})
         result['msg'] = 'Could not clean up'
